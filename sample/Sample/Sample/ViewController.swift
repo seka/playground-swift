b0e90e47f9db26c9100eda9f412eb20298db5933
seka <s.seka134704@gmail.com>
2022-05-26 16:37:07 +0900

    Sampleプロジェクトを追加

---
+++ sample/Sample/Sample/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by shin.sekaryo on 2022/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        debugPrint("test -----")
+    }
+
+
+}
+
