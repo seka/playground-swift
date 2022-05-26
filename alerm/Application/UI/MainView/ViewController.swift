26d7883a3b7ddb13cc74630d8d74c99efad72155
seka <s.seka134704@gmail.com>
2022-05-26 16:37:23 +0900

    Alermプロジェクトを追加

---
+++ alerm/Application/UI/MainView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alerm
+//
+//  Created by shin.sekaryo on 2022/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
