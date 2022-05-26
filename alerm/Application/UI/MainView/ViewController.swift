148053fbd78ff73f898ed8b24260760243182bc6
seka <s.seka134704@gmail.com>
2022-05-26 19:02:21 +0900

    レイアウトとコードを接続

--- alerm/Application/UI/MainView/ViewController.swift
+++ alerm/Application/UI/MainView/ViewController.swift
@@ -7,13 +7,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    @IBOutlet private var label: UILabel!
+    
+    @IBOutlet private var picker: UIDatePicker!
+    
+    @IBOutlet private var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
